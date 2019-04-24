16658cf247249ab13b8bb406e8f72121990b95e3
QianchengWu <254152970@qq.com>
2019-04-24 15:52:09 -0700

    matrix with multi-threads.

---
+++ hw2_matrix.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<math.h>
+#include<time.h>
+#include<vector>
+using namespace std;
+using matrix = vector<vector<int> >;
+
+// maximum size of matrix 
+#define MAX 50
+
+matrix matA(MAX, vector<int>(MAX, 0));
+matrix matB(MAX, vector<int>(MAX, 0));
+matrix matC(MAX, vector<int>(MAX, 0));
+
+void calculateMatrix(){
+
+    for (int i = 0; i < MAX; i++){
+        for (int j = 0; j < MAX; j++){
+            int sum = 0;
+            for (int k = 0; k < MAX;k++){
+                sum += matA[i][k] * matB[k][j];
+            }
+            matC[i][j] = sum;
+        }
+    }
+
+}
+
+void printMatrix(matrix c){
+    for(int i=0;i<c.size();i++){
+        for(int j=0;j<c[0].size();j++){
+            cout<<c[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    cout<<endl;
+}
+int main(){
+    
+	struct timespec start, finish;
+    double elapsed;
+
+     // Generating random values in matA and matB 
+    for (int i = 0; i < MAX; i++) { 
+        for (int j = 0; j < MAX; j++) { 
+            matA[i][j] = rand() % 100; 
+            matB[i][j] = rand() % 100; 
+        } 
+    } 
+
+    printMatrix(matA);
+    printMatrix(matB);
+
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    //code
+    calculateMatrix();
+
+    clock_gettime(CLOCK_MONOTONIC, &finish);
+
+    printMatrix(matC);
+
+    elapsed = (finish.tv_sec - start.tv_sec);
+    elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+
+    cout<<"Time: "<<elapsed<<"s"<<endl;
+
+    return 0;
+}
+
---
+++ hw2_matrix_thread.cpp
@@ -0,0 +1,98 @@
+#include<iostream>
+#include<time.h>
+#include<vector>
+#include<mutex>
+using namespace std;
+using matrix = vector<vector<int> >;
+  
+// maximum size of matrix 
+#define MAX 50
+  
+// maximum number of threads 
+#define MAX_THREAD 4
+  
+matrix matA(MAX, vector<int>(MAX, 0));
+matrix matB(MAX, vector<int>(MAX, 0));
+matrix matC(MAX, vector<int>(MAX, 0));
+int step_i = 0; 
+mutex m;
+
+void* multi(void* arg) 
+{   
+    m.lock();
+    int core = step_i++; 
+    m.unlock();
+
+    // Each thread computes 1/MAX th of matrix multiplication 
+    for (int i = core * MAX / MAX_THREAD; i < (core + 1) * MAX / MAX_THREAD; i++)  
+        for (int j = 0; j < MAX; j++)  
+            for (int k = 0; k < MAX; k++)  
+                matC[i][j] += matA[i][k] * matB[k][j]; 
+
+} 
+  
+// Driver Code 
+int main() 
+{   
+    struct timespec start, finish;
+    double elapsed;
+
+    // Generating random values in matA and matB 
+    for (int i = 0; i < MAX; i++) { 
+        for (int j = 0; j < MAX; j++) { 
+            matA[i][j] = rand() % 100; 
+            matB[i][j] = rand() % 100; 
+        } 
+    } 
+  
+    // Displaying matA 
+    cout << endl 
+         << "Matrix A" << endl; 
+    for (int i = 0; i < MAX; i++) { 
+        for (int j = 0; j < MAX; j++)  
+            cout << matA[i][j] << " "; 
+        cout << endl; 
+    } 
+  
+    // Displaying matB 
+    cout << endl 
+         << "Matrix B" << endl; 
+    for (int i = 0; i < MAX; i++) { 
+        for (int j = 0; j < MAX; j++)  
+            cout << matB[i][j] << " ";         
+        cout << endl; 
+    } 
+    
+    clock_gettime(CLOCK_MONOTONIC, &start);
+
+    // declaring four threads 
+    pthread_t threads[MAX_THREAD]; 
+  
+    // Creating four threads, each evaluating its own part 
+    for (int i = 0; i < MAX_THREAD; i++) { 
+        int* p; 
+        pthread_create(&threads[i], NULL, multi, (void*)(p)); 
+    } 
+  
+    // joining and waiting for all threads to complete 
+    for (int i = 0; i < MAX_THREAD; i++)  
+        pthread_join(threads[i], NULL);     
+    
+    clock_gettime(CLOCK_MONOTONIC, &finish);
+
+    elapsed = (finish.tv_sec - start.tv_sec);
+    elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+
+    // Displaying the result matrix 
+    cout << endl 
+         << "Multiplication of A and B" << endl; 
+    for (int i = 0; i < MAX; i++) { 
+        for (int j = 0; j < MAX; j++)  
+            cout << matC[i][j] << " ";         
+        cout << endl; 
+    }
+
+    cout << "\nTotal Time : " << elapsed << " s" << endl;
+
+    return 0; 
+} 
